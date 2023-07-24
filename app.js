97f6f696a601168a8fd0c444a719d80cf4c6b9f4
Mattlotus <mattlotus91@gmail.com>
2023-07-24 01:03:09 -0400

    final card

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
 
+let img = document.querySelector(".heartsTwo")
+let button = document.querySelector('.button')
+
+button.addEventListener('click', () => {
+    
+    img.src = 'https://e7.pngegg.com/pngimages/590/498/png-clipart-snoopy-wood-charlie-brown-peanuts-snoopy-s-love-white.png'
+})
 
 
-const open = () =>{
-    let card = document.querySelector('cardContainer')
-    let newImage = document.createElement('img')
-    newImage.setAttribute('src' ,'https://images.rawpixel.com/image_png_social_square/cHJpdmF0ZS9zdGF0aWMvaW1hZ2VzL3dlYnNpdGUvMjAyMi0xMC9ybTQ2Ny1oZWFydC0wMDFfMS5wbmc.png?s=aO5yviu6eYtr09KFppgRwJTNj2GkZlKZpjLh2DD3zOk' )
-    newImage.setAttribute('class,mainImage')
-    let ogImg  = document.querySelector('.mainImage')
-    ogImg.replaceWith(newImage)
-    let top = document.querySelector('.toptitle')
-    top.innerHTML = "I LOVE YOU"
-    let down = document.querySelector('.title')
-    down.innerHTML = "SPREAD LOVE"
-}
+
+
+
+// const open = () =>{
+//     let card = document.querySelector('cardContainer')
+//     let newImage = document.createElement('img')
+//     newImage.setAttribute('src' ,'https://www.citypng.com/public/uploads/preview/-51611552483wdglfftfqc.png' )
+//     newImage.setAttribute('class', 'heartsTwo')
+//     let ogImg  = document.querySelector('.heartsTwo')
+//     ogImg.replaceWith(newImage)
+
+// }
